077ba8d52c4418d929e2f49033d3af85dec3f81b
Yoonhwi <ush0105@naver.com>
2024-06-12 23:00:18 +0900

    added news-subview && ad-box

--- src/components/layout/centered-layout/centered-main/main-column.left/column-left.news/column-left.news.tsx
+++ src/components/layout/centered-layout/centered-main/main-column.left/column-left.news/column-left.news.tsx
@@ -1,5 +1,34 @@
 import Styles from "./index.module.css";
 
+const Companys = [
+  {
+    name: "뉴스타파",
+    img: "https://s.pstatic.net/static/newsstand/2020/logo/light/0604/930.png",
+  },
+  {
+    name: "한국일보",
+    img: "https://s.pstatic.net/static/newsstand/2020/logo/light/0604/038.png",
+  },
+  {
+    name: "서울신문",
+    img: "https://s.pstatic.net/static/newsstand/up/2022/0208/nsd16121208.png",
+  },
+  {
+    name: "KBS",
+    img: "https://s.pstatic.net/static/newsstand/2020/logo/light/0604/056.png",
+  },
+  {
+    name: "YTN",
+    img: "https://s.pstatic.net/static/newsstand/2020/logo/light/0604/052.png",
+  },
+  {
+    name: "뉴데일리",
+    img: "https://s.pstatic.net/static/newsstand/up/2024/0124/nsd95528325.png",
+  },
+];
+
+const repeatedCompanys = [...Companys, ...Companys, ...Companys, ...Companys];
+
 const ColumnHeader = [
   "뉴스스탠드",
   "언론사편집",
@@ -21,11 +50,32 @@ const ColumnLeftNews = () => {
         <div className={Styles.header_subview}>
           <div className={Styles.subview_category}>
             <text>전체언론사</text>
-            <div className={Styles.subview_triangle}></div>
+            <i className={Styles.subview_triangle} />
           </div>
           <text>
             연합뉴스 김도훈호 한국축구, 중국과 북중미 월드컵 예선 전반0-0
           </text>
+          <div className={Styles.subview_home}>뉴스스탠드 홈</div>
+        </div>
+
+        <div className={Styles.news_table}>
+          {repeatedCompanys.map((company, i) => {
+            return (
+              <div className={Styles.table_item} key={i}>
+                <img src={company.img} height={"20px"} alt={company.name} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className={Styles.row_bottom}>
+        <div className={Styles.paging_icon_area}>
+          <i className={Styles.paging_icon} />
+        </div>
+        <text>언론사 더보기 6/21</text>
+        <div className={Styles.paging_icon_area}>
+          <i className={Styles.paging_icon} style={{ transform: "none" }} />
         </div>
       </div>
     </div>
--- src/components/layout/centered-layout/centered-main/main-column.right/main-column.right.tsx
+++ src/components/layout/centered-layout/centered-main/main-column.right/main-column.right.tsx
@@ -4,7 +4,15 @@ const MainColumnRight = () => {
   return (
     <div className={Styles.outer_box}>
       <ColumnRightMyview />
-      <div className={Styles.ad_box}></div>
+      <div className={Styles.ad_box}>
+        <img
+          src="https://ssl.pstatic.net/melona/libs/1491/1491376/1781400040bea3eeea04_20240401144028928.jpg"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
       <div className={Styles.weather_box}></div>
       <div className={Styles.stock_box}></div>
       <div className={Styles.ad2_box}></div>
